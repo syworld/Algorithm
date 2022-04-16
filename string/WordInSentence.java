57a1396e906e08d0f5f0d67300b8e313d8dd995e
syworld <syworldy@gmail.com>
2022-04-16 13:50:22 +0900

    solve: string 문장 속 단어 substring() 메소드 사용

---
+++ string/WordInSentence.java
@@ -0,0 +1,51 @@
+/**
+ * 문장 속 가장 긴 단어
+ * it is time to study
+ * sTUdy
+ */
+
+import java.util.Scanner;
+
+public class WordInSentence {
+
+    public String solution(String str) {
+        String answer = "";
+
+        int maxWordLen = Integer.MIN_VALUE;
+        int pos;
+        while ((pos = str.indexOf(" ")) != -1) {
+            // 띄어쓰기 발견할 때까지 루프
+            String tmp = str.substring(0, pos);
+            int len = tmp.length();
+            if (len > maxWordLen) {
+                maxWordLen = len;
+                answer = tmp;
+            }
+            str = str.substring(pos+1);
+        }
+        // 마지막 단어 체크
+        if(str.length()>maxWordLen){
+            answer = str;
+        }
+
+
+//        String[] words = str.split(" ");
+//        for(String word : words){
+//            int wordLen = word.length();
+//            if(wordLen>maxWordLen){
+//                maxWordLen = wordLen;
+//                answer = word;
+//            }
+//        }
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        WordInSentence wordInSentence = new WordInSentence();
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.print(wordInSentence.solution(str));
+    }
+}
