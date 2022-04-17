f15d55309f090526bc3ffc027f1dc74428719bbe
syworld <syworldy@gmail.com>
2022-04-17 13:49:01 +0900

    solve: array 큰 수 출력

---
+++ array/BigNumber.java
@@ -0,0 +1,39 @@
+/**
+ * 큰 수 출력
+ * 7
+ * 4 6 2 21 4 56 5
+ * 4 6 21 56
+ */
+
+import java.util.*;
+
+public class BigNumber {
+
+  public ArrayList<Integer> solution(int[] arr, int n) {
+    ArrayList<Integer> answer = new ArrayList<>();
+    answer.add(arr[0]);
+    for (int i = 1; i < n; i++) {
+      if (arr[i] > arr[i - 1]) {
+        answer.add(arr[i]);
+      }
+    }
+
+    return answer;
+  }
+
+
+  public static void main(String[] args) {
+    BigNumber bigNumber = new BigNumber();
+    Scanner scanner = new Scanner(System.in);
+
+    int n = scanner.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scanner.nextInt();
+    }
+
+    for (int x : bigNumber.solution(arr, n)) {
+      System.out.print(x + " ");
+    }
+  }
+}
