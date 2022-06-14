40f59bca642f5dabf4a450628189cf4fc7296b3a
syworld <syworldy@gmail.com>
2022-06-15 02:33:56 +0900

    hash: substring 비교

--- Programmers/hash/Solution2.java
+++ Programmers/hash/Solution2.java
@@ -8,15 +8,29 @@ public class Solution2 {
   public boolean solution(String[] phone_book) {
     boolean answer = true;
 
-    // 길이 순서로 정렬
-    Arrays.sort(phone_book);
+    // Sol1) 길이 순서로 정렬
+//    Arrays.sort(phone_book);
+//
+//    for (int i = 0; i < phone_book.length - 1; i++) {
+//      if (phone_book[i + 1].startsWith(phone_book[i])) {
+//        answer = false;
+//        break;
+//      }
+//    }
 
-    for (int i = 0; i < phone_book.length - 1; i++) {
-      if (phone_book[i + 1].startsWith(phone_book[i])) {
-        answer = false;
-        break;
+    // Sol2) Hash
+    HashMap<String, Integer> map = new HashMap<>();
+
+    for(String phone_number: phone_book){
+      map.put(phone_number, 1);
+    }
+
+    for(String phone: phone_book){
+      for(int j=1; j<phone.length(); j++){
+        if(map.containsKey(phone.substring(0,j))) return false;
       }
     }
+
 
     return answer;
   }
