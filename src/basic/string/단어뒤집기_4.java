43c44b8c3736724a8763c250e5712bebddc27d5e
syworld <syworldy@gmail.com>
2022-07-01 11:35:12 +0900

    string: lt, rt 치환

---
+++ src/basic/string/단어뒤집기_4.java
@@ -0,0 +1,54 @@
+package basic.string;
+
+/** 단어 뒤집기
+ * 2
+ * good
+ * hello
+ * doog
+ * olleh
+ */
+import java.util.*;
+
+public class 단어뒤집기_4 {
+
+    public ArrayList<String> solution(String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+        // Solution #1: StringBuilder reverse() 사용
+//        for(String x: str){
+//            String tmp = new StringBuilder(x).reverse().toString();
+//            answer.add(tmp);
+//        }
+
+        for(String x: str){
+            // String을 문자 배열로 변환
+            char[] charArr = x.toCharArray();
+            int lt =0, rt= x.length() -1;
+            while(lt<rt){
+                char tmp = charArr[lt];
+                charArr[lt] = charArr[rt];
+                charArr[rt] = tmp;
+                lt++;
+                rt--;
+            }
+            // String으로 다시 변환
+            String tmp = String.valueOf(charArr);
+            answer.add(tmp);
+        }
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        단어뒤집기_4 reverse = new 단어뒤집기_4();
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        String[] str = new String[n];
+        for(int i=0; i<n; i++){
+            str[i] = scanner.next();
+        }
+        for(String x: reverse.solution(str)){
+            System.out.println(x);
+        }
+    }
+}
