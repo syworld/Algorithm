302b40bbcbb937b90930423ef8aa963a865f195d
syworld <syworldy@gmail.com>
2022-07-18 01:39:42 +0900

    simulation: 세로 읽기

---
+++ src/boj/Main10798.java
@@ -0,0 +1,29 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main10798 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    char[][] g = new char[5][15];
+
+    for(int i=0; i< g.length; i++){
+      String str = br.readLine();
+      for(int j=0; j<str.length(); j++){
+        g[i][j] = str.charAt(j);
+      }
+    }
+
+    for(int i=0; i<15;i++){
+      for(int j=0; j<5; j++){
+        if(g[j][i] == '\0') continue;
+        System.out.print(g[j][i]);
+      }
+    }
+  }
+
+}
