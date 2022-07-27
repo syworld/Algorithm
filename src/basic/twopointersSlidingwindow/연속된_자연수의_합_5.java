40627b6ae3185e5bf60db0b579fe22eba88f3bfb
syworld <syworldy@gmail.com>
2022-07-28 00:42:04 +0900

    two-pointers: 연속 합이 같은 경우의 수

---
+++ src/basic/twopointersSlidingwindow/연속된_자연수의_합_5.java
@@ -0,0 +1,41 @@
+package basic.twopointersSlidingwindow;
+
+import java.util.Scanner;
+
+public class 연속된_자연수의_합_5 {
+
+  public int solution(int n) {
+    int answer = 0, sum = 0, lt = 0;
+    int m = n / 2 + 1; // 절반 이상이면 합하면 무조건 n 이상
+    // sum = lt~rt까지의 연속 합
+    int[] num = new int[m];
+    for (int i = 0; i < m; i++) {
+      num[i] = i + 1;
+    }
+
+    for (int rt = 0; rt < m; rt++) {
+      // rt가 증가하고, 더하고, 비교
+      sum += num[rt];
+      if (sum == n) {
+        answer++;
+      }
+      while (sum >= n) {
+        // lt를 빼고, 증가하고, 비교
+        sum -= num[lt++];
+        if (sum == n) {
+          answer++;
+        }
+      }
+    }
+    return answer;
+  }
+
+
+  public static void main(String[] args) {
+    연속된_자연수의_합_5 T = new 연속된_자연수의_합_5();
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    System.out.println(T.solution(n));
+  }
+
+}
